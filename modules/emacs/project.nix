4b1971d62a189cee2525f130262d1061d08e5fb8
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-10-30 16:34:12 +0100

    emacs: compile: Move logic from project module

--- modules/emacs/project.nix
+++ modules/emacs/project.nix
@@ -28,7 +28,6 @@ with pkgs.lib.ordenada;
         name = "ordenada-project";
         config = ''
           (eval-when-compile
-            (require 'project)
             (require 'xdg))
           (defgroup ordenada-project nil
             "Custom `project.el' enhancements."
@@ -37,13 +36,6 @@ with pkgs.lib.ordenada;
             "List of root files that indicate a directory is a project."
             :group 'ordenada-project
             :type '(repeat string))
-
-          (defun ordenada-compilation-buffer-name (mode)
-            "Returns the result of `project-prefixed-buffer-name' if inside
-          project and `compilation--default-buffer-name' if not."
-              (if (project-current)
-                  (project-prefixed-buffer-name mode)
-                  (compilation--default-buffer-name mode)))
 
           (defun ordenada-project-custom-root (dir)
             "Search in project's DIR for a set of project dominating files."
@@ -58,29 +50,12 @@ with pkgs.lib.ordenada;
             "Determine the PROJECT root."
             (cdr project))
 
-          (defun ordenada-project-compile (&optional comint)
-            "Compile current project and choose if buffer will be in COMINT mode."
-            (interactive "P")
-            (let ((default-directory (project-root (project-current t)))
-                  (compilation-buffer-name-function
-                   (or project-compilation-buffer-name-function
-                       compilation-buffer-name-function)))
-              (call-interactively 'compile nil (and comint (vector (list 4))))))
-
           (setopt ordenada-project-dominating-files '(${
             toString (map (x: ''"${x}"'') user.features.emacs.project.extraDominatingFiles)
           }))
           (add-hook 'project-find-functions #'ordenada-project-custom-root)
           (add-hook 'project-find-functions #'project-try-vc)
-          (advice-add 'project-compile :override #'ordenada-project-compile)
           (with-eval-after-load 'project
-            (with-eval-after-load 'consult
-              (setq consult-project-root-function
-                    (lambda ()
-                      (when-let (project (project-current))
-                        (car (project-roots project))))))
-            (setq compilation-buffer-name-function #'ordenada-compilation-buffer-name)
-            (setq project-compilation-buffer-name-function #'ordenada-compilation-buffer-name))
             (keymap-set project-prefix-map "F" #'consult-find)
             (keymap-set project-prefix-map "R" #'consult-ripgrep)
             (setopt project-switch-use-entire-map t)
