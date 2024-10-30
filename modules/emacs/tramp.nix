82c7e9148124f431153ab7c428ceca215bbb191d
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-10-30 16:41:49 +0100

    emacs: tramp: Add ignore dir regexp setting to speed it up

--- modules/emacs/tramp.nix
+++ modules/emacs/tramp.nix
@@ -21,6 +21,10 @@ with pkgs.lib.ordenada;
           (with-eval-after-load 'tramp
             (setopt tramp-verbose 1)
             (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+          (with-eval-after-load 'vc
+            (setopt vc-ignore-dir-regexp
+                    (format "%s\\|%s"
+                            vc-ignore-dir-regexp tramp-file-name-regexp)))
         '';
       };
     });
