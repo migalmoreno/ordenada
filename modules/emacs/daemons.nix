5f1e89b38a93e646f25937fd2703f451c08e52a5
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-11-03 09:48:06 +0100

    emacs: daemons: Add fillFrame option

--- modules/emacs/daemons.nix
+++ modules/emacs/daemons.nix
@@ -11,6 +11,7 @@ with pkgs.lib.ordenada;
   options = {
     ordenada.features.emacs.daemons = {
       enable = lib.mkEnableOption "the Emacs daemons feature";
+      fillFrame = lib.mkEnableOption "showing the list of daemons in the current full frame";
     };
   };
   config = {
@@ -22,7 +23,7 @@ with pkgs.lib.ordenada;
             (keymap-set ordenada-app-map "D" #'daemons))
           (add-hook 'daemons-mode-hook #'eldoc-mode)
           (with-eval-after-load 'daemons
-            (setopt daemons-list-fill-frame t)
+            (setopt daemons-list-fill-frame ${if user.features.emacs.daemons.fillFrame then "t" else "nil"})
             (setopt daemons-systemd-is-user t))
         '';
         elispPackages = with pkgs.emacsPackages; [ daemons ];
