16dd4126da7040e68a04ecd9024ce8e0256770a8
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-11-18 23:50:26 +0100

    mkDocs: Trim last line from README in docs package

--- mkDocs.nix
+++ mkDocs.nix
@@ -42,19 +42,20 @@ pkgs.runCommand "ordenada-docs"
   ''
     tmpdir=$(mktemp -d)
     mkdir -p $out
+    sed '$ d' ${./README} > $tmpdir/README
 
     cat ${optionsDoc.optionsCommonMark} > $tmpdir/options.md
     cat $tmpdir/options.md > $out/options.md
 
     emacs --batch --eval \
       "(progn (require 'ox) (require 'ox-md) \
-       (find-file \"${./README}\") \
+       (find-file \"$tmpdir/README\") \
        (org-export-to-file 'md \"$tmpdir/readme.md\") (bury-buffer))"
     cat $tmpdir/readme.md $tmpdir/options.md > $out/index.md
 
     pandoc -f markdown -o $tmpdir/options.org $tmpdir/options.md
-    cat ${./README} $tmpdir/options.org > $out/index.org
-    cat ${./README} > $out/readme.org
+    cat $tmpdir/README $tmpdir/options.org > $out/index.org
+    cat $tmpdir/README > $out/readme.org
 
     pandoc -f markdown+smart -o $out/index.html $out/index.md
   ''
