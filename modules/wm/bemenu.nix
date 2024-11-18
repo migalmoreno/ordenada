f6a2e2a6ce95dd4845937fc9f526fa2ea4d41220
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-11-18 23:38:29 +0100

    wm: bemenu: Use common options for pinentry

--- modules/wm/bemenu.nix
+++ modules/wm/bemenu.nix
@@ -7,51 +7,79 @@
 
 with pkgs.lib.ordenada;
 
+let
+  inherit (lib) mkEnableOption mkOption types;
+  mkBemenuOpts =
+    opts:
+    toString (
+      lib.mapAttrsToList (
+        name: value:
+        "--${name} ${
+          if builtins.isInt value then
+            (toString value)
+          else if builtins.isBool value then
+            ""
+          else
+            "'${value}'"
+        }"
+      ) opts
+    );
+in
 {
   options = {
     ordenada.features.bemenu = {
-      enable = lib.mkEnableOption "the bemenu feature";
-      height = lib.mkOption {
-        type = lib.types.int;
-        description = "The height of the dmenu prompt.";
+      enable = mkEnableOption "the bemenu feature";
+      height = mkOption {
+        type = types.int;
+        description = "The height of the bemenu prompt.";
         default = 34;
       };
     };
   };
   config = {
-    home-manager = mkHomeConfig config "bemenu" (user: {
-      programs.bemenu = {
-        enable = true;
-        settings = with user.features.theme.scheme.withHashtag; {
-          line-height = 34;
-          ignorecase = true;
-          hp = 10;
-          cw = 1;
-          ch = 20;
-          tf = base05;
-          tb = base02;
-          ff = base05;
-          fb = base01;
-          nf = base05;
-          nb = base01;
-          af = base05;
-          ab = base01;
-          cf = base05;
-          cb = base01;
-          hf = base01;
-          hb = base0D;
-          fn = "${user.features.fontutils.fonts.monospace.name} 11";
+    home-manager = mkHomeConfig config "bemenu" (
+      user: with config.home-manager.users.${user.name}.programs.bemenu; {
+        programs.bemenu = {
+          enable = true;
+          settings = with user.features.theme.scheme.withHashtag; {
+            line-height = 34;
+            ignorecase = true;
+            hp = 10;
+            cw = 1;
+            ch = 20;
+            tf = base05;
+            tb = base02;
+            ff = base05;
+            fb = base01;
+            nf = base05;
+            nb = base01;
+            af = base05;
+            ab = base01;
+            cf = base05;
+            cb = base01;
+            hf = base01;
+            hb = base0D;
+            fn = with user.features.fontutils.fonts.monospace; "${name} ${toString size}";
+          };
         };
-      };
-      wayland.windowManager.sway.config.menu = with user.features.theme.scheme.withHashtag; ''
-        ${pkgs.j4-dmenu-desktop}/bin/j4-dmenu-desktop \
-         --dmenu="${pkgs.bemenu}/bin/bemenu -i -H ${toString user.features.bemenu.height} \
-         --fn '${user.features.fontutils.fonts.monospace.name} ${toString user.features.fontutils.fonts.monospace.size}' \
-         --hp 10 --cw 1 --ch 20 \
-         --tf '${base05}' --tb '${base02}' --ff '${base05}' --fb '${base01}' \
-         --nf '${base05}' --nb '${base01}' --af '${base05}' --ab '${base01}' \
-         --cf '${base05}' --cb '${base01}' --hf '${base01}' --hb '${base0D}'"
-      '';
-    });
+        services.gpg-agent.pinentryPackage = lib.mkForce (
+          pkgs.writeShellScriptBin "pinentry-bemenu" ''
+            BEMENU_OPTS= ${pkgs.pinentry-bemenu}/bin/pinentry-bemenu ${
+              mkBemenuOpts (
+                removeAttrs settings [
+                  "cw"
+                  "hp"
+                  "ch"
+                ]
+              )
+            }
+          ''
+        );
+        wayland.windowManager.sway.config.menu = ''
+          ${pkgs.j4-dmenu-desktop}/bin/j4-dmenu-desktop \
+           --dmenu="${pkgs.bemenu}/bin/bemenu ${mkBemenuOpts settings}"
+        '';
+      }
+    );
   };
 }
