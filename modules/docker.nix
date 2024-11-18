e2451142e7462fcf825b814632be61947abdeb98
Miguel Ángel Moreno <mail@migalmoreno.com>
2024-11-18 23:35:51 +0100

    docker: Add Emacs configuration

--- modules/docker.nix
+++ modules/docker.nix
@@ -8,12 +8,18 @@
 with pkgs.lib.ordenada;
 
 let
+  inherit (lib) mkEnableOption mkOption types;
   cfg = config.ordenada.features.docker;
 in
 {
   options = {
     ordenada.features.docker = {
-      enable = lib.mkEnableOption "the Docker feature";
+      enable = mkEnableOption "the Docker feature";
+      key = mkOption {
+        type = types.str;
+        default = "D";
+        description = "Keybinding to launch Emacs Docker interface.";
+      };
     };
   };
   config = lib.mkMerge [
@@ -21,6 +27,20 @@ in
     {
       users = mkHomeConfig config "docker" (user: {
         extraGroups = [ "docker" ];
+      });
+      home-manager = mkHomeConfig config "docker" (user: {
+        programs.emacs = mkElispConfig {
+          name = "ordenada-docker";
+          config = ''
+            (with-eval-after-load 'ordenada-keymaps
+              (keymap-set ordenada-app-map "${user.features.docker.key}" #'docker))
+            (add-to-list 'auto-mode-alist '(".*Dockerfile\\'" . dockerfile-mode))
+          '';
+          elispPackages = with pkgs.emacsPackages; [
+            docker
+            dockerfile-mode
+          ];
+        };
       });
     }
   ];
