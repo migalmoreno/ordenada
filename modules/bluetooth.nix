8ea16691171773855b1651b725175c3f93fe6448
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-01-22 11:41:11 +0100

    bluetooth: Remove battery display warning

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -29,7 +29,8 @@ in
             (with-eval-after-load 'ordenada-keymaps
               (keymap-set ordenada-app-map "B" #'bluetooth-list-devices))
             (with-eval-after-load 'bluetooth
-              (keymap-set bluetooth-mode-map "C" #'bluetooth-connect-profile))
+              (keymap-set bluetooth-mode-map "C" #'bluetooth-connect-profile)
+              (setopt bluetooth-battery-display-warning nil))
           '';
           elispPackages = with pkgs.emacsPackages; [ bluetooth ];
         };
