409218740971bf5200cb6591f85e6926b48aead6
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-04-06 22:36:38 +0200

    chore(age): shorten syntax

--- modules/age.nix
+++ modules/age.nix
@@ -11,40 +11,32 @@ let
   inherit (lib) types mkOption mkEnableOption;
 in
 {
-  options = {
-    ordenada.features.age = {
-      enable = mkEnableOption "the age feature";
-      package = mkOption {
-        type = types.package;
-        default = pkgs.age;
-        description = "The age package to use.";
-      };
-      identities = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        description = "The list of age identities.";
-      };
-      recipients = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        description = "The list of age recipients.";
-      };
+  options.ordenada.features.age = {
+    enable = mkEnableOption "the age feature";
+    package = mkPackageOption pkgs "age" { };
+    identities = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = "The list of age identities.";
+    };
+    recipients = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = "The list of age recipients.";
     };
   };
-  config = {
-    home-manager = mkHomeConfig config "age" (user: {
-      home.packages = [ user.features.age.package ];
-      programs.emacs = mkElispConfig {
-        name = "ordenada-age";
-        config = with user.features.age; ''
-          (require 'age)
-          (age-file-enable)
-          (setopt age-program "${lib.getExe package}")
-          (setopt age-default-identity ${mkList identities})
-          (setopt age-default-recipient ${mkList recipients})
-        '';
-        elispPackages = with pkgs.emacsPackages; [ age ];
-      };
-    });
-  };
+  config.home-manager = mkHomeConfig config "age" (user: {
+    home.packages = [ user.features.age.package ];
+    programs.emacs = mkElispConfig {
+      name = "ordenada-age";
+      config = with user.features.age; ''
+        (require 'age)
+        (age-file-enable)
+        (setopt age-program "${lib.getExe package}")
+        (setopt age-default-identity ${mkList identities})
+        (setopt age-default-recipient ${mkList recipients})
+      '';
+      elispPackages = with pkgs.emacsPackages; [ age ];
+    };
+  });
 }
