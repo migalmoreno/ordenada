048c4d5574d5a538fe9487ac819329a7996624c1
Demis Balbach <db@minikn.xyz>
2025-05-27 17:50:37 +0200

    feat(home):  Creates .profile if no shell module is used

--- modules/home.nix
+++ modules/home.nix
@@ -1,17 +1,11 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 
 with pkgs.lib.ordenada;
 
 let
   inherit (lib) mkOption types;
   cfg = config.ordenada.features.home;
-in
-{
+in {
   options = {
     ordenada.features.home = {
       enable = mkEnableTrueOption "the home feature";
@@ -32,7 +26,8 @@ in
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.backupFileExtension = "backup";
-      
+
+      ## Starting the chosen wm on the desired tty if enabled
       environment.loginShellInit = lib.mkIf (cfg.autoStartWmOnTty != null) ''
         [[ $(tty) == ${cfg.autoStartWmOnTty} ]] && exec ${config.ordenada.globals.wm}
       '';
@@ -42,7 +37,17 @@ in
         programs.home-manager.enable = true;
         targets.genericLinux.enable = true;
         home.stateVersion = "24.05";
+
+        ## If the user isn't using any of the shell modules, add the session-vars to .profile
+        ## ourselves so other modules work properly 
+        home.file.".profile".text =
+          lib.mkIf (config.ordenada.globals.shell == null) ''
+            . "${
+              config.home-manager.users.${user.name}.home.profileDirectory
+            }/etc/profile.d/hm-session-vars.sh"
+          '';
       });
+
       users = mkHomeConfig config "home" (user: {
         isNormalUser = true;
         extraGroups = [ "wheel" ] ++ user.features.home.extraGroups;
