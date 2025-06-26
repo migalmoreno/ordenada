d42930d11ce39b4cd4d1b556a9ddd84f83a5b40e
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-06-26 10:13:24 +0200

    feat(ssh): remove system configuration

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -20,12 +20,7 @@ in
         description = "The SSH stanzas to use in the client configuration.";
         default = { };
       };
-      rootAuthorizedKeys = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        description = "List of root authorized SSH keys.";
-        default = [ ];
-      };
-      userAuthorizedKeys = lib.mkOption {
+      authorizedKeys = lib.mkOption {
         type = lib.types.listOf lib.types.str;
         description = "List of user authorized SSH keys.";
         default = cfg.rootAuthorizedKeys;
@@ -33,23 +28,9 @@ in
     };
   };
   config = lib.mkMerge [
-    (lib.mkIf (cfg.enable && cfg.daemon) {
-      services.openssh = {
-        enable = true;
-        settings = {
-          PasswordAuthentication = false;
-          KbdInteractiveAuthentication = false;
-          PermitRootLogin = "yes";
-        };
-      };
-      networking.firewall.allowedTCPPorts = [ 22 ];
-      users.users.root = {
-        openssh.authorizedKeys.keys = cfg.rootAuthorizedKeys;
-      };
-    })
     {
       users = mkHomeConfig config "ssh" (user: {
-        openssh.authorizedKeys.keys = user.features.ssh.userAuthorizedKeys;
+        openssh.authorizedKeys.keys = user.features.ssh.authorizedKeys;
       });
       home-manager = mkHomeConfig config "ssh" (user: {
         services.ssh-agent.enable =
