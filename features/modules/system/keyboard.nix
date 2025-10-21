52daa844ef59f84f9ec8920399a8d235c03ad376
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-10-21 22:38:51 +0200

    feat: migrate fontutils and keyboard features

---
+++ features/modules/system/keyboard.nix
@@ -0,0 +1,44 @@
+{ lib, mkFeature, ... }:
+
+let
+  inherit (lib) types mkOption;
+in
+mkFeature {
+  name = "keyboard";
+  options = {
+    layout = mkOption {
+      type = types.submodule {
+        options = {
+          name = mkOption {
+            type = types.str;
+            description = "The XKB name of the keyboard layout.";
+            default = "us";
+          };
+          variant = mkOption {
+            type = types.str;
+            description = "The XKB layout variant name.";
+            default = "";
+          };
+          options = mkOption {
+            type = types.listOf types.str;
+            description = "The list of XKB options to use for this layout.";
+            default = [ ];
+          };
+        };
+      };
+      description = "The keyboard layout.";
+      default = {
+        name = "us";
+        options = [ "ctrl:nocaps" ];
+      };
+    };
+  };
+  homeManager =
+    { config, ... }:
+    {
+      home.keyboard = with config.ordenada.features.keyboard; {
+        inherit options variant;
+        layout = name;
+      };
+    };
+}
