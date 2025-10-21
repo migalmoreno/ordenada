659bbc5bb42f5fe70b3109a6d9e18c79cf62e2c0
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-10-21 22:33:47 +0200

    feat: add options for all classes and add globals

--- features/mk-feature.nix
+++ features/mk-feature.nix
@@ -1,69 +1,66 @@
-args@{
+{
   name,
   options ? { },
-  classOptions ? { },
+  globals ? { },
+  nixos ? { },
+  homeManager ? { },
+  darwin ? { },
   ...
 }:
 let
   module =
     { config, lib, ... }:
     let
-      opts = {
-        ordenada.features.${name} = {
-          enable = lib.mkEnableOption "the ${name} feature";
-        } // options;
-      };
-      mkClassConfig =
-        class: module: extraModules:
-        lib.optionalAttrs (module != null) {
-          ${class} =
-            args@{ config, pkgs, ... }:
-            {
-              imports = extraModules;
-              options = lib.recursiveUpdate opts (
-                lib.optionalAttrs (builtins.hasAttr class classOptions) {
-                  ordenada.features.${name} =
-                    if builtins.isFunction classOptions.${class} then
-                      classOptions.${class} args
-                    else
-                      classOptions.${class};
+      fname = if builtins.isList name then name else [ name ];
+      mkClassConfig = class: module: extraModules: {
+        ${class} =
+          args@{ config, pkgs, ... }:
+          let
+            mod = if builtins.isFunction module then (module args) else module;
+          in
+          {
+            imports = extraModules ++ lib.optionals (builtins.hasAttr "imports" mod) mod.imports;
+            options = lib.recursiveUpdate (lib.optionalAttrs (builtins.hasAttr "options" mod) mod.options) {
+              ordenada.features = lib.setAttrByPath fname (
+                {
+                  enable = lib.mkEnableOption "the ${name} feature";
                 }
-              );
-              config = lib.mkIf config.ordenada.features.${name}.enable (
-                if builtins.isFunction module then module args else module
+                // (if builtins.isFunction options then options args else options)
               );
             };
-        };
+            config = lib.mkMerge [
+              {
+                ordenada.globals = if builtins.isFunction globals then (globals args) else globals;
+              }
+              (lib.mkIf (lib.attrByPath fname false config.ordenada.features).enable (
+                removeAttrs mod [
+                  "imports"
+                  "options"
+                ]
+              ))
+            ];
+          };
+      };
+      homeModule = [
+        (
+          { config, ... }:
+          {
+            config = lib.mkIf config.ordenada.features.home.enable {
+              home-manager.sharedModules = [
+                {
+                  ordenada.features = lib.setAttrByPath fname { enable = lib.mkDefault true; };
+                }
+              ];
+            };
+          }
+        )
+      ];
     in
     {
-      config = lib.mkMerge (
-        lib.mapAttrsToList
-          (class: module: {
-            ordenada.modules.${name} = mkClassConfig class module (
-              lib.optionals (class == "nixos" || class == "darwin") [
-                (
-                  { config, ... }:
-                  {
-                    config = lib.mkIf config.ordenada.features.home.enable {
-                      home-manager.sharedModules = [
-                        {
-                          ordenada.features.${name}.enable = lib.mkDefault true;
-                        }
-                      ];
-                    };
-                  }
-                )
-              ]
-            );
-          })
-          (
-            removeAttrs args [
-              "name"
-              "options"
-              "classOptions"
-            ]
-          )
-      );
+      config.ordenada.modules.${builtins.concatStringsSep "-" fname} =
+        mkClassConfig "nixos" nixos homeModule
+        // mkClassConfig "homeManager" homeManager [ ]
+        // mkClassConfig "darwin" darwin homeModule;
     };
 in
 {
