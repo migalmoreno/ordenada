4c7937877dc7ebb59e6a4899774d0cba9dc983be
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-10-25 00:29:48 +0200

    feat(pipewire): add missing emacs configuration

--- modules/system/pipewire.nix
+++ modules/system/pipewire.nix
@@ -1,4 +1,4 @@
-{ mkFeature, ... }:
+{ mkFeature, ordenada-lib, ... }:
 
 mkFeature {
   name = "pipewire";
@@ -15,6 +15,21 @@ mkFeature {
       home.packages = [ pkgs.pulseaudio ];
       home.sessionVariables = {
         RTC_USE_PIPEWIRE = "true";
+      };
+      programs.emacs = ordenada-lib.mkElispConfig pkgs {
+        name = "ordenada-pulseaudio-control";
+        config = ''
+          (with-eval-after-load 'ordenada-keymaps
+            (keymap-set ordenada-app-map "v" #'pulseaudio-control-map))
+          (with-eval-after-load 'pulseaudio-control
+            (keymap-set pulseaudio-control-map "L"
+              #'pulseaudio-control-toggle-sink-input-mute-by-index)
+            (setopt pulseaudio-control-volume-step "5%")
+            (setopt pulseaudio-control-volume-verbose nil)
+            (pulseaudio-control-default-sink-mode)
+            (pulseaudio-control-default-source-mode))
+        '';
+        elispPackages = with pkgs.emacsPackages; [ pulseaudio-control ];
       };
     };
 }
