ebf8c162360a467a6ae81a298dc7e0cd5cd9eebe
Demis Balbach <db@minikn.xyz>
2025-11-05 00:58:09 +0100

    feat(json): Implement flymake backend for JSON5 using jsonlint

--- modules/lang/json.nix
+++ modules/lang/json.nix
@@ -81,8 +81,10 @@ mkFeature {
       programs.emacs = ordenada-lib.mkElispConfig pkgs {
         name = "ordenada-json";
         config = ''
+          (require 'cl-lib)
           (require 'json5-ts-mode)
 
+          ;; setup
           (defgroup ordenada-json nil
             "General JSON/JSON5 programming utilities."
             :group 'ordenada)
@@ -102,6 +104,66 @@ mkFeature {
           (add-to-list 'auto-mode-alist '("\\.jsonc\\'" . jsonc-mode))
           (add-to-list 'auto-mode-alist '("\\.json5\\'" . json5-ts-mode))
 
+          ;; flymake
+          (defvar-local ordenada-json--flymake-proc nil)
+
+          (defun ordenada-json-flymake (report-fn &rest _args)
+            (when (process-live-p ordenada-json--flymake-proc)
+              (kill-process ordenada-json--flymake-proc))
+
+            (let* ((source (current-buffer))
+                   (mode (with-current-buffer source
+                           (let* ((filename (buffer-file-name))
+                                  (ext (when filename (file-name-extension filename))))
+                           (cond
+                             ((string-equal ext "json5") "json5")
+                             ((string-equal ext "jsonc") "cjson")
+                             (t "json"))))))
+              (save-restriction
+                (widen)
+                (setq
+                ordenada-json--flymake-proc
+                (make-process
+                   :name "ordenada-json-flymake"
+                   :noquery t
+                   :connection-type 'pipe
+                   :buffer (generate-new-buffer " *json-flymake*")
+                   :command `("${prantlf-jsonlint}/bin/jsonlint" "-c" "-q" "-M" ,mode)
+                   :sentinel
+                   (lambda (proc _event)
+                     (when (memq (process-status proc) '(exit signal))
+                       (unwind-protect
+                         (if (with-current-buffer source (eq proc ordenada-json--flymake-proc))
+                           (with-current-buffer (process-buffer proc)
+                             (goto-char (point-min))
+                             (cl-loop
+                               while (search-forward-regexp
+                                       "^\\([^:]+\\): line \\([0-9]+\\), col \\([0-9]+\\), \\(.+?\\)\\.$"
+                                       nil t)
+                               for (beg . end) = (flymake-diag-region
+                                                   source
+                                                   (string-to-number (match-string 2))
+                                                   (string-to-number (match-string 3)))
+                               for msg = (match-string 4)
+                               for type = :error
+                               when (and beg end)
+                               collect (flymake-make-diagnostic source
+                                                                beg
+                                                                end
+                                                                type
+                                                                msg)
+                               into diags
+                               finally (funcall report-fn diags)))
+                             (flymake-log :warning "Canceling obsolete check %s"
+                                     proc))
+                         (kill-buffer (process-buffer proc)))))))
+          (process-send-region ordenada-json--flymake-proc (point-min) (point-max))
+          (process-send-eof ordenada-json--flymake-proc))))
+
+          (defun ordenada-json--flymake-setup-backend ()
+            (add-hook 'flymake-diagnostic-functions 'ordenada-json-flymake nil t))
+
+          ;; minor modes
           (define-minor-mode ordenada-json-mode
             "Set up convenient tweaks for JSON/JSONC development."
             :group 'ordenada-json :keymap ordenada-json-mode-map
@@ -116,13 +178,19 @@ mkFeature {
             "Set up convenient tweaks for JSON5 development."
             :group 'ordenada-json :keymap ordenada-json5-mode-map
             (when ordenada-json5-mode
+              (setq json5-ts-mode-indent-offset 2)
+
               (ordenada-json--setup-electric-pairs-for-json)
-              (setq json5-ts-mode-indent-offset 2)))
+
+              (add-hook 'flymake-diagnostic-functions 'ordenada-json-flymake nil t)
+              (flymake-mode 1)
+            ))
           (add-hook 'json5-ts-mode-hook 'ordenada-json5-mode)
 
           (keymap-set ordenada-json-mode-map "C-c f"
                       '("Format buffer" . json-pretty-print-buffer))
 
+          ;; eglot
           (with-eval-after-load 'eglot
             (setq-default eglot-workspace-configuration
                           '(:json (:format (:enable t) ;; doesn't work
