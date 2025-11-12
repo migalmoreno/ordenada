4c2bb33e06742e84be3e00887c7269f80fd88155
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-11-13 00:24:55 +0100

    feat(nix): add nix-ts-mode and adapt config

--- modules/lang/nix.nix
+++ modules/lang/nix.nix
@@ -8,7 +8,7 @@
 mkFeature {
   name = "nix";
   options = {
-    enablePolymode = lib.mkEnableOption "Polymode support for Nix multi-line strings";
+    enablePolymode = ordenada-lib.mkEnableTrueOption "Polymode support for Nix multi-line strings";
   };
   nixos = {
     nix = {
@@ -30,14 +30,17 @@ mkFeature {
         name = "ordenada-nix";
         config = # elisp
           ''
-            (add-hook 'nix-mode-hook #'eglot-ensure)
+            (add-hook 'nix-ts-mode-hook #'eglot-ensure)
             (with-eval-after-load 'eglot
-              (add-to-list 'eglot-server-programs '(nix-mode . ("nil"))))
+              (add-to-list 'eglot-server-programs '(nix-ts-mode . ("nil"))))
+
+            (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-ts-mode))
+
             ${lib.optionalString config.ordenada.features.nix.enablePolymode ''
               (eval-and-compile
                 (require 'polymode))
               (define-hostmode ordenada-nix-hostmode
-                :mode 'nix-mode)
+                :mode 'nix-ts-mode)
               (define-auto-innermode ordenada-nix-dynamic-innermode
                 :head-matcher (cons "^.*\\(#[[:blank:]]+[a-z-]+\n[[:blank:]]*'''\\)" 1)
                 :tail-matcher "^[ \t]+'''[ \n]*)?;"
@@ -52,16 +55,25 @@ mkFeature {
               (add-to-list 'auto-mode-alist '("\\.nix" . ordenada-nix-polymode))
 
               (with-eval-after-load 'rainbow-delimiters
-                (add-hook 'nix-mode-hook #'rainbow-delimiters-mode-disable))
+                (add-hook 'nix-ts-mode-hook #'rainbow-delimiters-mode-disable))
             ''}
             (with-eval-after-load 'smartparens
-              (add-hook 'nix-mode-hook #'turn-off-smartparens-mode))
+              (add-hook 'nix-ts-mode-hook #'turn-off-smartparens-mode))
 
-            (add-hook 'nix-mode-hook #'electric-pair-local-mode)
+            (add-hook 'nix-ts-mode-hook #'electric-pair-local-mode)
           '';
         elispPackages =
           with pkgs.emacsPackages;
-          [ nix-mode ] ++ (lib.optional config.ordenada.features.nix.enablePolymode polymode);
+          [
+            nix-mode
+            nix-ts-mode
+            (treesit-grammars.with-grammars (
+              grammars: with grammars; [
+                tree-sitter-nix
+              ]
+            ))
+          ]
+          ++ (lib.optional config.ordenada.features.nix.enablePolymode polymode);
       };
     };
 }
