d69587b26093d2de70a85404aa418817581c7d20
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-11-15 17:46:15 +0100

    feat(android): add fdroid.el configuration

--- modules/development/android.nix
+++ modules/development/android.nix
@@ -1,7 +1,17 @@
-{ mkFeature, ... }:
+{
+  lib,
+  mkFeature,
+  ordenada-lib,
+  ...
+}:
 
 mkFeature {
   name = "android";
+  options.fdroidKey = lib.mkOption {
+    type = lib.types.str;
+    description = "Keybinding for fdroid.el map operations.";
+    default = "f";
+  };
   nixos =
     { pkgs, ... }:
     {
@@ -12,7 +22,20 @@ mkFeature {
       ordenada.features.userInfo.extraGroups = [ "adbusers" ];
     };
   homeManager =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
+    let
+      emacs-fdroid = pkgs.emacsPackages.melpaBuild {
+        pname = "fdroid";
+        version = "0.1.1";
+        src = pkgs.fetchFromGitHub {
+          owner = "migalmoreno";
+          repo = "fdroid.el";
+          rev = "94c2011630886011e15d37c163f7f59f2a045db0";
+          hash = "sha256-6bSxjZOAZ8PpiaqbKdE2elobGgwmt1B8KZ7I9f+mhh0=";
+        };
+        packageRequires = with pkgs.emacsPackages; [ embark ];
+      };
+    in
     {
       home.packages = with pkgs; [
         android-tools
@@ -20,5 +43,16 @@ mkFeature {
         fdroidcl
       ];
       wayland.windowManager.sway.config.floating.criteria = [ { app_id = "Waydroid"; } ];
+      programs.emacs = ordenada-lib.mkElispConfig pkgs {
+        name = "ordenada-android";
+        config = # elisp
+          ''
+            (with-eval-after-load 'ordenada-keymaps
+              (keymap-set ordenada-app-map "${config.ordenada.features.android.fdroidKey}" 'fdroid-map))
+          '';
+        elispPackages = [
+          emacs-fdroid
+        ];
+      };
     };
 }
