386061e54235acdead06425472ac93f1e0e51f42
Miguel Ángel Moreno <mail@migalmoreno.com>
2025-11-15 17:41:07 +0100

    chore(emacs-spelling): add elisp mode comment and examples

--- modules/editor/emacs/spelling.nix
+++ modules/editor/emacs/spelling.nix
@@ -28,21 +28,29 @@ mkFeature {
         type = types.str;
         description = "Default dictionary for ispell.";
         default = "";
+        example = "en_US";
       };
       ispellPersonalDictionary = mkOption {
         type = types.str;
         description = "Personal dictionary for ispell.";
         default = "";
+        example = "en_US";
       };
       flyspellHooks = mkOption {
         type = types.listOf types.str;
         description = "List of mode hooks where flyspell-mode should be enabled.";
         default = [ ];
+        example = [
+          "org-mode-hook"
+          "message-mode-hook"
+          "bibtex-mode-hook"
+        ];
       };
       flyspellProgHooks = mkOption {
         type = types.listOf types.str;
         description = "List of mode hooks where flyspell-prog-mode should be enabled.";
         default = [ ];
+        example = [ "prog-mode-hook" ];
       };
       dictionaryServer = mkOption {
         type = types.str;
@@ -61,31 +69,33 @@ mkFeature {
       home.packages = [ config.ordenada.features.emacs.spelling.package ];
       programs.emacs = ordenada-lib.mkElispConfig pkgs {
         name = "ordenada-spelling";
-        config = with config.ordenada.features.emacs.spelling; ''
-          (mapcar (lambda (hook)
-                    (add-hook hook 'flyspell-mode))
-                  '(${toString flyspellHooks}))
-          (mapcar (lambda (hook)
-                    (add-hook hook 'flyspell-prog-mode))
-                  '(${toString flyspellProgHooks}))
-          (with-eval-after-load 'ispell
-            (setopt ispell-program-name "${ispellProgram}")
-            ${lib.optionalString (ispellStandardDictionary != "") ''
-              (setopt ispell-dictionary "${ispellStandardDictionary}")
-            ''}
-            ${
-              lib.optionalString (ispellPersonalDictionary != "") ''
-                (setopt ispell-personal-dictionary "${ispellPersonalDictionary}")
-              ''
-            })
-          (with-eval-after-load 'flyspell
-            (setopt flyspell-issue-welcome-flag nil)
-            (setopt flyspell-issue-message-flag nil))
-          (with-eval-after-load 'dictionary
-            (setopt dictionary-server "${dictionaryServer}"))
-          (with-eval-after-load 'ordenada-keymaps
-            (keymap-set ordenada-app-map "${dictionaryKey}" #'dictionary-search))
-        '';
+        config =
+          with config.ordenada.features.emacs.spelling; # elisp
+          ''
+            (mapcar (lambda (hook)
+                      (add-hook hook 'flyspell-mode))
+                    '(${toString flyspellHooks}))
+            (mapcar (lambda (hook)
+                      (add-hook hook 'flyspell-prog-mode))
+                    '(${toString flyspellProgHooks}))
+            (with-eval-after-load 'ispell
+              (setopt ispell-program-name "${ispellProgram}")
+              ${lib.optionalString (ispellStandardDictionary != "") ''
+                (setopt ispell-dictionary "${ispellStandardDictionary}")
+              ''}
+              ${
+                lib.optionalString (ispellPersonalDictionary != "") ''
+                  (setopt ispell-personal-dictionary "${ispellPersonalDictionary}")
+                ''
+              })
+            (with-eval-after-load 'flyspell
+              (setopt flyspell-issue-welcome-flag nil)
+              (setopt flyspell-issue-message-flag nil))
+            (with-eval-after-load 'dictionary
+              (setopt dictionary-server "${dictionaryServer}"))
+            (with-eval-after-load 'ordenada-keymaps
+              (keymap-set ordenada-app-map "${dictionaryKey}" #'dictionary-search))
+          '';
       };
     };
 }
