52900be241af704dcd7e3861309dd60c2fdc7a6f
Demis Balbach <db@Demiss-MacBook-Pro.local>
2025-11-19 15:55:11 +0100

    feat(xdg): Make userDirs work with darwin

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -6,6 +6,38 @@ mkFeature {
     { config, ... }:
     let
       inherit (lib) mkOption types;
+      maybeCapitalize =
+        path:
+        let
+          capitalize =
+            s:
+            if lib.stringLength s < 1 then
+              s
+            else
+              (lib.toUpper (lib.substring 0 1 s)) + (lib.substring 1 (lib.stringLength s - 1) s);
+          components = lib.splitString "/" path;
+          lastNonEmptyIndex =
+            let
+              findLastIndex =
+                idx:
+                if idx < 0 then
+                  -1
+                else if lib.elemAt components idx != "" then
+                  idx
+                else
+                  findLastIndex (idx - 1);
+            in
+            findLastIndex (lib.length components - 1);
+          modifiedComponents = lib.genList (
+            i:
+            let
+              original = lib.elemAt components i;
+            in
+            if i == lastNonEmptyIndex then capitalize original else original
+          ) (lib.length components);
+          modifiedPath = lib.concatStringsSep "/" modifiedComponents;
+        in
+        if (config.ordenada.globals.platform == "darwin") then modifiedPath else path;
     in
     {
       baseDirs = mkOption {
@@ -23,13 +55,13 @@ mkFeature {
         description = "The XDG user directories.";
         default = with config.ordenada.features.userInfo; {
           desktop = null;
-          documents = "${homeDirectory}/documents";
-          download = "${homeDirectory}/downloads";
-          music = "${homeDirectory}/music";
-          pictures = "${homeDirectory}/pictures";
-          publicShare = "${homeDirectory}/public";
+          documents = maybeCapitalize "${homeDirectory}/documents";
+          download = maybeCapitalize "${homeDirectory}/downloads";
+          music = maybeCapitalize "${homeDirectory}/music";
+          pictures = maybeCapitalize "${homeDirectory}/pictures";
+          publicShare = maybeCapitalize "${homeDirectory}/public";
           templates = null;
-          videos = "${homeDirectory}/videos";
+          videos = maybeCapitalize "${homeDirectory}/videos";
         };
       };
     };
@@ -59,10 +91,9 @@ mkFeature {
           configHome = lib.mkForce baseDirs.configHome;
           dataHome = lib.mkForce baseDirs.dataHome;
           stateHome = lib.mkForce baseDirs.stateHome;
-          userDirs = userDirs;
         };
       })
-      (lib.mkIf (platform == "linux") {
+      (lib.mkIf (platform == "nixos") {
         home.packages = with pkgs; [ xdg-utils ];
         xdg = {
           mime.enable = true;
@@ -79,13 +110,30 @@ mkFeature {
               common.default = [
                 "gtk"
               ]
-              ++ lib.optonal (wayland == true) "wlr";
+              ++ lib.optional (wayland == true) "wlr";
             };
           };
-          userDirs = {
-            enable = true;
-            createDirectories = true;
-          };
+          userDirs =
+            with config.ordenada.features.xdg;
+            lib.mkMerge [
+              userDirs
+              {
+                enable = true;
+                createDirectories = true;
+              }
+            ];
+        };
+      })
+      (with config.ordenada.features.xdg.userDirs; lib.mkIf (platform == "darwin") {
+        home.sessionVariables = {
+          #XDG_DESKTOP_DIR = lib.mkIf (desktop != null) desktop;
+          XDG_DOCUMENTS_DIR = lib.mkIf (documents != null) documents;
+          XDG_DOWNLOAD_DIR = lib.mkIf (download != null) download;
+          XDG_MUSIC_DIR = lib.mkIf (music != null) music;
+          XDG_PICTURES_DIR = lib.mkIf (pictures != null) pictures;
+          XDG_PUBLICSHARE_DIR = lib.mkIf (publicShare != null) publicShare;
+          #XDG_TEMPLATES_DIR = lib.mkIf (templates != null) templates;
+          XDG_VIDEOS_DIR = lib.mkIf (videos != null) videos;
         };
       })
     ];
