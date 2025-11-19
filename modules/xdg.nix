02d8146503fdc3e6ae1c3dacb74cab66df983445
Demis Balbach <db@Demiss-MacBook-Pro.local>
2025-11-19 14:20:21 +0100

    feat(xdg): apply linux only features only on linux platform; take wayland into consideration

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -34,13 +34,13 @@ mkFeature {
       };
     };
   nixos =
-    { pkgs, ... }:
+    { config, pkgs, ... }:
     {
-      xdg.portal = {
+      xdg.portal = with config.ordenada.globals; {
         enable = true;
-        config.common.default = "wlr";
+        config.common.default = if (wayland == true) then "wlr" else "gtk";
         extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-        wlr.enable = true;
+        wlr.enable = wayland;
       };
     };
   homeManager =
@@ -50,38 +50,43 @@ mkFeature {
       pkgs,
       ...
     }:
-    {
-      home.packages = with pkgs; [ xdg-utils ];
-      xdg = lib.mkMerge [
-        (with config.ordenada.features.xdg; {
+    with config.ordenada.globals;
+    lib.mkMerge [
+      (with config.ordenada.features.xdg; {
+        xdg = {
           enable = true;
-          mime.enable = true;
-          mimeApps.enable = true;
-          portal = {
-            enable = true;
-            extraPortals = with pkgs; [
-              xdg-desktop-portal-gtk
-              xdg-desktop-portal-wlr
-            ];
-            config = {
-              common.default = [
-                "gtk"
-                "wlr"
-              ];
-            };
-          };
           cacheHome = lib.mkForce baseDirs.cacheHome;
           configHome = lib.mkForce baseDirs.configHome;
           dataHome = lib.mkForce baseDirs.dataHome;
           stateHome = lib.mkForce baseDirs.stateHome;
           userDirs = userDirs;
-        })
-        {
+        };
+      })
+      (lib.mkIf (platform == "linux") {
+        home.packages = with pkgs; [ xdg-utils ];
+        xdg = {
+          mime.enable = true;
+          mimeApps.enable = true;
+          portal = {
+            enable = true;
+            extraPortals =
+              with pkgs;
+              [
+                xdg-desktop-portal-gtk
+              ]
+              ++ lib.optional (wayland == true) xdg.desktop-portal-wlr;
+            config = {
+              common.default = [
+                "gtk"
+              ]
+              ++ lib.optonal (wayland == true) "wlr";
+            };
+          };
           userDirs = {
             enable = true;
             createDirectories = true;
           };
-        }
-      ];
-    };
+        };
+      })
+    ];
 }
