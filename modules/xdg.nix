c666052ae2eaa22228c26b320434ef64bd1ad609
Demis Balbach <db@minikn.xyz>
2025-11-21 09:55:49 +0100

    feat(xdg): simplify darwin dir logic; add apply to dir types

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -1,4 +1,9 @@
-{ lib, mkFeature, ... }:
+{
+  lib,
+  mkFeature,
+  ordenada-lib,
+  ...
+}:
 
 mkFeature {
   name = "xdg";
@@ -6,63 +11,58 @@ mkFeature {
     { config, ... }:
     let
       inherit (lib) mkOption types;
-      maybeCapitalize =
+      mkMaybeCapitalizedDir =
         path:
         let
-          capitalize =
-            s:
-            if lib.stringLength s < 1 then
-              s
-            else
-              (lib.toUpper (lib.substring 0 1 s)) + (lib.substring 1 (lib.stringLength s - 1) s);
-          components = lib.splitString "/" path;
-          lastNonEmptyIndex =
+          pred =
+            config.ordenada.features.xdg.capitalizeUserDirs
+            && builtins.isString path
+            && lib.strings.hasInfix "/" path;
+          result =
             let
-              findLastIndex =
-                idx:
-                if idx < 0 then
-                  -1
-                else if lib.elemAt components idx != "" then
-                  idx
-                else
-                  findLastIndex (idx - 1);
+              components = lib.splitString "/" path;
+              dirname = lib.lists.init components;
+              basename = lib.lists.last components;
             in
-            findLastIndex (lib.length components - 1);
-          modifiedComponents = lib.genList (
-            i:
-            let
-              original = lib.elemAt components i;
-            in
-            if i == lastNonEmptyIndex then capitalize original else original
-          ) (lib.length components);
-          modifiedPath = lib.concatStringsSep "/" modifiedComponents;
+            "${lib.concatStringsSep "/" dirname}/${ordenada-lib.str.capitalize basename}";
         in
-        if (config.ordenada.globals.platform == "darwin") then modifiedPath else path;
+        if (pred) then result else path;
     in
     {
-      baseDirs = mkOption {
-        type = types.attrs;
-        description = "The XDG base directories.";
-        default = with config.ordenada.features.userInfo; {
-          configHome = "${homeDirectory}/.config";
-          dataHome = "${homeDirectory}/.local/share";
-          cacheHome = "${homeDirectory}/.cache";
-          stateHome = "${homeDirectory}/.local/state";
+      baseDirs =
+        with config.ordenada.features.userInfo;
+        mkOption {
+          type = ordenada-lib.types.fnOrAttrs;
+          description = "The XDG base directories.";
+          apply = x: ordenada-lib.getFnOrAttrsValue x ({ homeDirectory = homeDirectory; });
+          default = {
+            configHome = "${homeDirectory}/.config";
+            dataHome = "${homeDirectory}/.local/share";
+            cacheHome = "${homeDirectory}/.cache";
+            stateHome = "${homeDirectory}/.local/state";
+          };
         };
-      };
-      userDirs = mkOption {
-        type = types.attrs;
-        description = "The XDG user directories.";
-        default = with config.ordenada.features.userInfo; {
-          desktop = null;
-          documents = maybeCapitalize "${homeDirectory}/documents";
-          download = maybeCapitalize "${homeDirectory}/downloads";
-          music = maybeCapitalize "${homeDirectory}/music";
-          pictures = maybeCapitalize "${homeDirectory}/pictures";
-          publicShare = maybeCapitalize "${homeDirectory}/public";
-          templates = null;
-          videos = maybeCapitalize "${homeDirectory}/videos";
+      userDirs =
+        with config.ordenada.features.userInfo;
+        mkOption {
+          type = ordenada-lib.types.fnOrAttrs;
+          description = "The XDG user directories.";
+          apply = x: ordenada-lib.getFnOrAttrsValue x ({ homeDirectory = homeDirectory; });
+          default = {
+            desktop = null;
+            documents = mkMaybeCapitalizedDir "${homeDirectory}/documents";
+            download = mkMaybeCapitalizedDir "${homeDirectory}/downloads";
+            music = mkMaybeCapitalizedDir "${homeDirectory}/music";
+            pictures = mkMaybeCapitalizedDir "${homeDirectory}/pictures";
+            publicShare = mkMaybeCapitalizedDir "${homeDirectory}/public";
+            templates = null;
+            videos = mkMaybeCapitalizedDir "${homeDirectory}/videos";
+          };
         };
+      capitalizeUserDirs = mkOption {
+        type = types.bool;
+        description = "Whether to capitalize the `userDirs` directory names.";
+        default = config.ordenada.globals.platform == "darwin";
       };
     };
   nixos =
@@ -83,8 +83,9 @@ mkFeature {
       ...
     }:
     with config.ordenada.globals;
+    with config.ordenada.features.xdg;
     lib.mkMerge [
-      (with config.ordenada.features.xdg; {
+      {
         xdg = {
           enable = true;
           cacheHome = lib.mkForce baseDirs.cacheHome;
@@ -92,7 +93,7 @@ mkFeature {
           dataHome = lib.mkForce baseDirs.dataHome;
           stateHome = lib.mkForce baseDirs.stateHome;
         };
-      })
+      }
       (lib.mkIf (platform == "nixos") {
         home.packages = with pkgs; [ xdg-utils ];
         xdg = {
@@ -124,17 +125,23 @@ mkFeature {
             ];
         };
       })
-      (with config.ordenada.features.xdg.userDirs; lib.mkIf (platform == "darwin") {
-        home.sessionVariables = {
-          #XDG_DESKTOP_DIR = lib.mkIf (desktop != null) desktop;
-          XDG_DOCUMENTS_DIR = lib.mkIf (documents != null) documents;
-          XDG_DOWNLOAD_DIR = lib.mkIf (download != null) download;
-          XDG_MUSIC_DIR = lib.mkIf (music != null) music;
-          XDG_PICTURES_DIR = lib.mkIf (pictures != null) pictures;
-          XDG_PUBLICSHARE_DIR = lib.mkIf (publicShare != null) publicShare;
-          #XDG_TEMPLATES_DIR = lib.mkIf (templates != null) templates;
-          XDG_VIDEOS_DIR = lib.mkIf (videos != null) videos;
-        };
-      })
+      (
+        ## TODO: Create directories if they don't exist
+        with config.ordenada.features.xdg.userDirs;
+        lib.mkIf (platform == "darwin") {
+          home.sessionVariables = (
+            lib.attrsets.filterAttrs (_: v: v != null) {
+              XDG_DESKTOP_DIR = desktop;
+              XDG_DOCUMENTS_DIR = documents;
+              XDG_DOWNLOAD_DIR = download;
+              XDG_MUSIC_DIR = music;
+              XDG_PICTURES_DIR = pictures;
+              XDG_PUBLICSHARE_DIR = publicShare;
+              XDG_TEMPLATES_DIR = templates;
+              XDG_VIDEOS_DIR = videos;
+            }
+          );
+        }
+      )
     ];
 }
