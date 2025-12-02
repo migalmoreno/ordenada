36717c08285e124f0facc22788522362432a6772
Demis Balbach <db@minikn.xyz>
2025-12-03 00:02:32 +0100

    feat(networking): add nameservers option

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,10 +1,24 @@
-{ mkFeature, ... }:
+{ lib, mkFeature, ... }:
 
+let
+  inherit (lib) mkEnableOption mkOption types;
+in
 mkFeature {
   name = "networking";
-  nixos = {
+  options = {
+    ordenada.features.networking = {
+      enable = mkEnableOption "the networking feature";
+      nameservers = mkOption {
+        description = "List of nameservers to use.";
+        type = types.listOf types.str;
+        default = [ ];
+      };
+    };
+  };
+  nixos = {config, ...}: with config.ordenada.features.networking; {
     networking.useDHCP = false;
     networking.networkmanager.enable = true;
+    networking.nameservers = nameservers;
     ordenada.features.userInfo.extraGroups = [ "networkmanager" ];
     programs.nm-applet = {
       enable = true;
