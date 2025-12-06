c781a3ea29ef686302b0c5e4f2d2e451600870a3
Demis Balbach <db@minikn.xyz>
2025-12-06 11:23:23 +0100

    feat(aerospace): fix keybindings

--- modules/wm/aerospace.nix
+++ modules/wm/aerospace.nix
@@ -5,15 +5,19 @@
   ...
 }:
 
+let
+  inherit (lib)
+    mkOption
+    mkPackageOption
+    types
+    ;
+in
 mkFeature {
   name = "aerospace";
   options =
     { config, pkgs, ... }:
     with config.ordenada.globals;
     with config.ordenada.features.xdg;
-    let
-      inherit (lib) mkOption mkPackageOption types;
-    in
     {
       package = mkPackageOption pkgs "aerospace" { };
       modifier = mkOption {
@@ -87,101 +91,110 @@ mkFeature {
           enable = true;
           keepAlive = true;
         };
+
+        settings = lib.recursiveUpdate {
+          config-version = 2;
+          enable-normalization-flatten-containers = true;
+          enable-normalization-opposite-orientation-for-nested-containers = true;
+          default-root-container-layout = "tiles";
+          default-root-container-orientation = "auto";
+
+          on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+          automatically-unhide-macos-hidden-apps = true;
+
+          gaps.inner = {
+            horizontal = 12;
+            vertical = 12;
+          };
+
+          persistent-workspaces = [
+            "1"
+            "2"
+            "3"
+            "4"
+            "5"
+            "6"
+            "7"
+            "8"
+            "9"
+            "0"
+          ];
+
+          mode.main.binding =
+            with config.ordenada.globals.apps;
+            lib.mkOptionDefault (
+              { }
+              // lib.optionalAttrs (terminal != null) {
+                "${modifier}-enter" = "exec-and-forget ${terminal}";
+              }
+              // lib.optionalAttrs (launcher != null) {
+                "${modifier}-d" = "exec-and-forget ${launcher}";
+              }
+              // lib.optionalAttrs (passwordManager != null) {
+                "${modifier}-p" = "exec-and-forget ${passwordManager}";
+              }
+              // {
+                "${modifier}-shift-q" = "close";
+
+                "${modifier}-${left}" = "focus left";
+                "${modifier}-${down}" = "focus down";
+                "${modifier}-${up}" = "focus up";
+                "${modifier}-${right}" = "focus right";
+
+                "${modifier}-shift-${left}" = "move left";
+                "${modifier}-shift-${down}" = "move down";
+                "${modifier}-shift-${up}" = "move up";
+                "${modifier}-shift-${right}" = "move right";
+
+                "${modifier}-b" = "join-with right";
+                "${modifier}-shift-b" = "join-with left";
+                "${modifier}-v" = "join-with down";
+                "${modifier}-shift-v" = "join-with up";
+                "${modifier}-f" = "fullscreen";
+
+                "${modifier}-s" = "layout tiles horizontal vertical";
+                "${modifier}-w" = "layout accordion horizontal vertical";
+                "${modifier}-shift-space" = "layout tiling floating";
+
+                "${modifier}-r" = "mode resize";
+
+                "${modifier}-1" = "workspace 1";
+                "${modifier}-2" = "workspace 2";
+                "${modifier}-3" = "workspace 3";
+                "${modifier}-4" = "workspace 4";
+                "${modifier}-5" = "workspace 5";
+                "${modifier}-6" = "workspace 6";
+                "${modifier}-7" = "workspace 7";
+                "${modifier}-8" = "workspace 8";
+                "${modifier}-9" = "workspace 9";
+                "${modifier}-0" = "workspace 10";
+
+                "${modifier}-shift-1" = "move-node-to-workspace 1";
+                "${modifier}-shift-2" = "move-node-to-workspace 2";
+                "${modifier}-shift-3" = "move-node-to-workspace 3";
+                "${modifier}-shift-4" = "move-node-to-workspace 4";
+                "${modifier}-shift-5" = "move-node-to-workspace 5";
+                "${modifier}-shift-6" = "move-node-to-workspace 6";
+                "${modifier}-shift-7" = "move-node-to-workspace 7";
+                "${modifier}-shift-8" = "move-node-to-workspace 8";
+                "${modifier}-shift-9" = "move-node-to-workspace 9";
+                "${modifier}-shift-0" = "move-node-to-workspace 0";
+
+                "${modifier}-shift-c" = "reload-config";
+              }
+              // extraKeybindings
+            );
+
+          mode.resize.binding = {
+            "${left}" = "resize width -50";
+            "${right}" = "resize width +50";
+            "${up}" = "resize height -50";
+            "${down}" = "resize height +50";
+            "equal" = "balance-sizes";
+            "esc" = "mode main";
+          };
+        } extraConfig;
       };
-userSettings =
-          lib.recursiveUpdate {
-            enable-normalization-flatten-containers = true;
-            enable-normalization-opposite-orientation-for-nested-containers = true;
-            default-root-container-layout = "tiles";
-            default-root-container-orientation = "auto";
-
-            on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
-            automatically-unhide-macos-hidden-apps = true;
-
-            key-mapping = {
-              preset = "qwerty";
-            };
-
-            gaps = {
-              inner.horizontal = 8;
-              inner.vertical = 8;
-              outer.left = 8;
-              outer.bottom = 8;
-              outer.top = 8;
-              outer.right = 8;
-            };
-
-            mode.main.binding = {
-              "${modifier}-enter" = mkIf (
-                config.ordenada.globals.terminal != null
-              ) "exec-and-forget ${config.ordenada.globals.terminal}";
-
-              "${modifier}-d" = mkIf (
-                config.ordenada.globals.launcher != null
-              ) "exec-and-forget ${config.ordenada.globals.launcher}";
-
-              "${modifier}-p" = mkIf (
-                config.ordenada.globals.passwordManager != null
-              ) "exec-and-forget ${config.ordenada.globals.passwordManager}";
-
-              "${modifier}-shift-q" = "close";
-
-              "${modifier}-${left}" = "focus left";
-              "${modifier}-${down}" = "focus down";
-              "${modifier}-${up}" = "focus up";
-              "${modifier}-${right}" = "focus right";
-
-              "${modifier}-shift-${left}" = "move left";
-              "${modifier}-shift-${down}" = "move down";
-              "${modifier}-shift-${up}" = "move up";
-              "${modifier}-shift-${right}" = "move right";
-
-              # "${modifier}+b" = "splith";
-              # "${modifier}+v" = "splitv";
-              "${modifier}-f" = "fullscreen";
-              # "${modifier}+a" = "focus parent";
-
-              "${modifier}-s" = "layout tiles horizontal vertical";
-              "${modifier}-w" = "layout accordion horizontal vertical";
-              "${modifier}-shift-space" = "layout tiling floating";
-
-              "${modifier}-r" = "mode resize";
-
-              "${modifier}-1" = "workspace 1";
-              "${modifier}-2" = "workspace 2";
-              "${modifier}-3" = "workspace 3";
-              "${modifier}-4" = "workspace 4";
-              "${modifier}-5" = "workspace 5";
-              "${modifier}-6" = "workspace 6";
-              "${modifier}-7" = "workspace 7";
-              "${modifier}-8" = "workspace 8";
-              "${modifier}-9" = "workspace 9";
-              "${modifier}-0" = "workspace 10";
-
-              "${modifier}-shift-1" = "move-node-to-workspace 1";
-              "${modifier}-shift-2" = "move-node-to-workspace 2";
-              "${modifier}-shift-3" = "move-node-to-workspace 3";
-              "${modifier}-shift-4" = "move-node-to-workspace 4";
-              "${modifier}-shift-5" = "move-node-to-workspace 5";
-              "${modifier}-shift-6" = "move-node-to-workspace 6";
-              "${modifier}-shift-7" = "move-node-to-workspace 7";
-              "${modifier}-shift-8" = "move-node-to-workspace 8";
-              "${modifier}-shift-9" = "move-node-to-workspace 9";
-              "${modifier}-shift-0" = "move-node-to-workspace 0";
-
-              "${modifier}-shift-c" = "reload-config";
-            };
-
-            mode.resize.binding = {
-              "${left}" = "resize width -50";
-              "${right}" = "resize width +50";
-              "${up}" = "resize height -50";
-              "${down}" = "resize height +50";
-              "equal" = "balance-sizes";
-              "esc" = "mode main";
-            };
-          }
-          // extraConfig;
 
     };
 }
