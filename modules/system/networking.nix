837ae1050f4e02755368bf1901eb72473e6c8a16
Demis Balbach <db@minikn.xyz>
2025-12-06 11:57:06 +0100

    feat(networking): fix options

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -6,23 +6,24 @@ in
 mkFeature {
   name = "networking";
   options = {
-    ordenada.features.networking = {
-      enable = mkEnableOption "the networking feature";
-      nameservers = mkOption {
-        description = "List of nameservers to use.";
-        type = types.listOf types.str;
-        default = [ ];
+    enable = mkEnableOption "the networking feature";
+    nameservers = mkOption {
+      description = "List of nameservers to use.";
+      type = types.listOf types.str;
+      default = [ ];
+    };
+  };
+  nixos =
+    { config, ... }:
+    with config.ordenada.features.networking;
+    {
+      networking.useDHCP = false;
+      networking.networkmanager.enable = true;
+      networking.nameservers = nameservers;
+      ordenada.features.userInfo.extraGroups = [ "networkmanager" ];
+      programs.nm-applet = {
+        enable = true;
+        indicator = true;
       };
     };
-  };
-  nixos = {config, ...}: with config.ordenada.features.networking; {
-    networking.useDHCP = false;
-    networking.networkmanager.enable = true;
-    networking.nameservers = nameservers;
-    ordenada.features.userInfo.extraGroups = [ "networkmanager" ];
-    programs.nm-applet = {
-      enable = true;
-      indicator = true;
-    };
-  };
 }
