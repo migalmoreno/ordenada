f68fb97ae52e35a3315b3854f7183cfd163abf47
Demis Balbach <db@minikn.xyz>
2025-12-30 23:58:18 +0100

    revert: move package option and global to darwin class
    Refs: dc67f55

--- modules/wm/aerospace.nix
+++ modules/wm/aerospace.nix
@@ -19,6 +19,7 @@ mkFeature {
     with config.ordenada.globals;
     with config.ordenada.features.xdg;
     {
+      package = mkPackageOption pkgs "aerospace" { };
       modifier = mkOption {
         type = types.str;
         description = "The modifier to bind aerospace keys to.";
@@ -67,13 +68,10 @@ mkFeature {
         description = "Extra Aerospace configuration.";
       };
     };
-  darwin =
-    { config, pkgs, ... }:
+  globals =
+    { config, ... }:
     {
-      options.ordenada = {
-        features.aerospace.package = mkPackageOption pkgs "aerospace" { };
-        globals.apps.wm = "${config.ordenada.features.aerospace.package}/Applications/AeroSpace.app";
-      };
+      apps.wm = "${config.ordenada.features.aerospace.package}/Applictions/AeroSpace.app";
     };
   homeManager =
     {
