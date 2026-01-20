481fefd3afd5b96a72bad4888195882649eaf05e
Demis Balbach <db@minikn.xyz>
2026-01-20 22:09:48 +0100

    chore: disable most android feature options on darwin

--- examples/features-darwin.nix
+++ examples/features-darwin.nix
@@ -1,5 +1,27 @@
 {
   ordenada.features = {
+
+    # development
+    android = {
+      enable = true;
+    ## fails due to lack of space
+    #   allowUnfreeAndAcceptLicenses = true;
+    #   activeSdkVersion = "35";
+    #   sdks = [
+    #     { platformVersion = "35"; }
+    #     { platformVersion = "36"; }
+    #   ];
+    #   emulators = [
+    #     {
+    #       name = "Small Phone";
+    #       platformVersion = "35";
+    #       persistentData = true;
+    #     }
+    #     {
+    #       platformVersion = "36";
+    #     }
+    #   ];
+    };
     aerospace.enable = true;
     homebrew.enable = true;
     docker.enable = true;
